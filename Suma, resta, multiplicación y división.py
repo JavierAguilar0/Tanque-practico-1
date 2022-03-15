3c535adfd3efe1e8197a7688ca196de61306f4b0
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-14 23:21:26 -0600

    Create Suma, resta, multiplicación y división.py

---
+++ Suma, resta, multiplicación y división.py
@@ -0,0 +1,11 @@
+# Capítulo 7:Suma, resta, multiplicación y división.
+# Ejercicio 16
+suma=20+23+25+19
+# Ejercicio 17
+resta=20-23-25-24-26-9
+# Ejercicio 18
+multiplicacion=20*23*1.89130435
+# Ejercicio 19
+division = 5000 / 230 / 2.17
+# Ejercicio 20
+operacion = 10 / 5 + 15 - 17

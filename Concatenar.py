e74347cea3fb8a880a6c9d76e0f0f4e1f4927def
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-14 22:52:09 -0600

    Create Concatenar.py

---
+++ Concatenar.py
@@ -0,0 +1,19 @@
+# Capítulo 4: Concatenar.
+# Ejercicio 7
+nombre="Javier "+"Aguilar"
+print(nombre)
+# Ejercicio 8
+nombre="Javier Aguilar"
+apodo="'cero'"
+nombreapodo=nombre + apodo
+# Ejercicio 9
+texto1="Daku"
+print(texto1+"Cero")
+# Ejercicio 10
+nombre="Javier"
+apellido1="Aguilar"
+apellido2="Corona"
+nombre_completo=nombre+" "+apellido1+" "+apellido2
+print(nombre_completo)
+# Ejercicio 11
+numero="21"+"15"

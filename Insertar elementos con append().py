4210009d818056915c5dc8518aa7caa12af987bf
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 15:49:47 -0600

    Create Insertar elementos con append().py

---
+++ Insertar elementos con append().py
@@ -0,0 +1,6 @@
+# Capítulo 15:Insertar elementos con append().
+# Ejercicio 30
+colores=['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+colores.append('fuxia')
+colores.append('celeste')
+print(colores)

39fd3e449257d8514d2cbf4927847a011f7bef20
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 15:56:41 -0600

    Create Ordenar elementos con sort().py

---
+++ Ordenar elementos con sort().py
@@ -0,0 +1,5 @@
+# Capítulo 17:Ordenar elementos con sort().
+# Ejercicio 32
+colores=['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+colores.sort(reverse=True)
+print(colores)

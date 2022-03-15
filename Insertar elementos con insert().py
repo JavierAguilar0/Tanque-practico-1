1f5e11c831791813d7d9782e5c960f90a3bcac6a
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 15:53:42 -0600

    Create Insertar elementos con insert().py

---
+++ Insertar elementos con insert().py
@@ -0,0 +1,6 @@
+# Capítulo 16:Insertar elementos con insert().
+# Ejercicio 31
+colores=['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+colores.insert(-4, 'magenta')
+colores.insert(-1, 'turquesa')
+print(colores)

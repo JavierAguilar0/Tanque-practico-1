629929da7ac937b958e75016a9f59a022abae130
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-14 22:42:07 -0600

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,7 @@
+#Capítulo 3: Strings.
+# Ejercicio 5
+nombre='Mi nombre es javier'
+juego="Mi juego favorito es Halo"
+# Ejercicio 6
+frase='"print()" se utiliza para imprimir valores en la consola'
+print(frase)

23870022e8282330210ee2d883b862e8aa4cdf90
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 15:35:02 -0600

    Create Posiciones negativas.py

---
+++ Posiciones negativas.py
@@ -0,0 +1,8 @@
+# Capítulo 11:Posiciones negativas.
+# Ejercicio 26
+colores=['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+print(colores[-1])
+print(colores[-7])
+print(colores[-5])
+print(colores[-2])
+print(colores[-10])

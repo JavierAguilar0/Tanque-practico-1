15a236514cb2e0352d1ac6c581e251caf41d8119
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 15:47:11 -0600

    Create Eliminar elementos con pop().py

---
+++ Eliminar elementos con pop().py
@@ -0,0 +1,7 @@
+# Capítulo 14:Eliminar elementos con pop().
+# Ejercicio 29
+colores=['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+primer_color=colores.pop(1)
+segundo_color=colores.pop(7)
+colores_guardados=[primer_color,segundo_color]
+print(colores_guardados)

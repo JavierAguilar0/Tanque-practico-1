9a2a0d750f9954b85dfc81856aa66b0de2ddab19
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 15:43:00 -0600

    Create Eliminar elementos con remove().py

---
+++ Eliminar elementos con remove().py
@@ -0,0 +1,6 @@
+# Capítulo 13:Eliminar elementos con remove().
+# Ejercicio 28
+colores=['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+colores.remove('rojo')
+colores.remove('amarillo')
+print(colores)

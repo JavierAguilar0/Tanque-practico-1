277ab6ffe29c7b27d5836122e79662c4ac6a40c8
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 16:02:13 -0600

    Create Crear y manejar tuplas y diferencias entre listas.py

---
+++ Crear y manejar tuplas y diferencias entre listas.py
@@ -0,0 +1,8 @@
+# Capítulo 19:Crear y manejar tuplas y diferencias entre listas.
+# Ejercicio 33
+colores=('rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja')
+print(colores[1])
+# Ejercicio 34
+numeros=(10,1,5,11)
+operacion=numeros[0]+numeros[2]+numeros[3]-numeros[1]
+print(operacion)

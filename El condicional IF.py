d45e53f2e1149178f9a90a21743393ef66b8c961
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 16:10:34 -0600

    Create El condicional IF.py

---
+++ El condicional IF.py
@@ -0,0 +1,16 @@
+# Capítulo 21:El condicional IF.
+# Ejercicio 36
+num1 = 15
+num2 = 20
+if num1 <= num2:
+	print('Se ejecuta el if.')
+# Ejercicio 37
+num1 = 1450
+num2 = 60
+if num1 > num2:
+	print('Se ejecuta el if.')
+# Ejercicio 38
+num1 = 1450
+num2 = 1450
+if num1 != num2:
+	print('Se ejecuta el if.')

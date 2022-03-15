d2a235dc77e896b50aad1918c82ffc0f586c5bbd
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 16:05:42 -0600

    Create Cómo convertir tuplas a listas y viceversa.py

---
+++ Cómo convertir tuplas a listas y viceversa.py
@@ -0,0 +1,5 @@
+# Capítulo 20:Cómo convertir tuplas a listas y viceversa.
+# Ejercicio 35
+colores=['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+colores2=tuple(colores)
+print(type(colores2))

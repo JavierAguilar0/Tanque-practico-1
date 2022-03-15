2e7dd831de6223e418c1971fe6007dd01f16dc1b
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-14 23:32:10 -0600

    Create Cómo crear listas y utilizarlas.py

---
+++ Cómo crear listas y utilizarlas.py
@@ -0,0 +1,9 @@
+# Capítulo 10:	Cómo crear listas y utilizarlas.
+# Ejercicio 23
+colores=["rojo","azul","verde","amarillo","marrón","lila","negro","rosa"]
+print(colores[3])
+# Ejercicio 24
+print(colores[0])
+print(colores[7])
+# Ejercicio 25
+numeros=["tres","dos","cinco","cuatro","uno"]

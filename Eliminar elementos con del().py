ae12aa50cb71c276ed71a7a82ea0229fca7e33cb
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 15:40:07 -0600

    Create Eliminar elementos con del().py

---
+++ Eliminar elementos con del().py
@@ -0,0 +1,8 @@
+# Capítulo 12:Eliminar elementos con del().
+# Ejercicio 27
+colores=['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+del colores[-9]
+del colores[3]
+del colores[4]
+del colores[4]
+print(colores)

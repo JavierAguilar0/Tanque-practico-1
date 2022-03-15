8ade55ec9547d41fd2a84240db14f43db4700b50
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-14 23:00:33 -0600

    Create Los métodos upper(), lower() y title().py

---
+++ Los métodos upper(), lower() y title().py
@@ -0,0 +1,7 @@
+# Capítulo 5:Los métodos upper(), lower() y title().
+# Ejercicio 12
+nombre="enrique barros fernández".title()
+# Ejercicio 13
+nombre="Esta Es Una Frase Para Ser Formateada.".lower()
+# Ejercicio 14
+nombre = "Esta Es Una Frase Para Ser Formateada.".upper()

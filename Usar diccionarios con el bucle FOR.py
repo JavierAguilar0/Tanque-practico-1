b25ef0a6a791910e2864c1a2ea75d3f0db50765c
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 19:03:01 -0600

    Create Usar diccionarios con el bucle FOR.py

---
+++ Usar diccionarios con el bucle FOR.py
@@ -0,0 +1,9 @@
+# Capítulo 32:Usar diccionarios con el bucle FOR.
+# Ejercicio 49
+teclado1={
+	'Categoría':'Teclados',
+	'Modelo':'HyperX Alloy FPS Pro',
+	'Precio':'89,99'
+}
+for x in teclado1:
+    print(x,"=",teclado1[x]+".")

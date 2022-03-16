345d82c9d5bb2141416d0a9c5414319a75ad92cb
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 18:05:43 -0600

    Create El bucle WHILE.py

---
+++ El bucle WHILE.py
@@ -0,0 +1,16 @@
+# Capítulo 27:El bucle WHILE.
+# Ejercicio 42
+x=0
+while x<=15:
+	print(x)
+	x+=5
+# Ejercicio 43
+x=0
+while x>=-100:
+    print(x)
+    x-=20
+# Ejercicio 43
+x=10
+while x>=0:
+    print("El valor del bucle es ",x)
+    x-=1

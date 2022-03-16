113b1ed143cad03ba6d3c7b1c8a5ba95296fef6f
JavierAguilar0 <javieraguilarog@outlook.com>
2022-03-15 18:58:13 -0600

    Create El método dict().py

---
+++ El método dict().py
@@ -0,0 +1,8 @@
+# Capítulo 31:El método dict().
+# Ejercicio 48
+teclado2={
+	'Categoría':'Teclados',
+	'Modelo':'Corsair K55 RGB',
+	'Precio':'59,99'
+}
+print('El modelo',teclado2['Modelo'],'cuesta',teclado2['Precio'],'$.')
